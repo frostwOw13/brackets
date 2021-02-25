ade1a9937c54d2ca4a227576eb5fcc15015b221e
frostwOw13 <frostwow13@yandex.ru>
2021-02-25 18:59:37 +0300

    feat: finish the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let end = false;
+  while (str.length != 0 && !end) {
+      end = true;
+      for (const e of bracketsConfig) {
+          let pair = e.join('');
+          if (str.includes(pair)) {
+              str = str.replace(pair, '');
+              end = false;
+          }
+      }
+  } 
+  return str.length == 0; 
 }
